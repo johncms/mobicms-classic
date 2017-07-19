d77ed8f0c67462c92bfadb23d8855708d7009c8e
Oleg Kasyanov <dev@mobicms.net>
2017-07-19 20:39:35 +0400

    Доработан класс Img

--- system/src/Asset/Img.php
+++ system/src/Asset/Img.php
@@ -2,40 +2,57 @@
 
 namespace Mobicms\Asset;
 
+/**
+ * Class Img
+ *
+ * @package Mobicms\Asset
+ * @author  Oleg Kasyanov <dev@mobicms.net>
+ *
+ * @method $this alt(string $alt)
+ * @method $this height(int $height)
+ * @method $this lowsrc(string $lowsrc)
+ * @method $this width(int $width)
+ * @method $this usemap(string $usemap)
+ *
+ * @method $this class(string $class)
+ * @method $this id(string $id)
+ * @method $this style(string $style)
+ * @method $this title(string $title)
+ */
 class Img
 {
-    private $image;
+    private $attribute;
 
     public function __construct($src)
     {
-        $this->image['src'] = 'src="' . $src . '"';
+        $this->attribute['src'] = 'src="' . $src . '"';
     }
 
-    public function alt($alt)
+    /**
+     * @param $name
+     * @param $arguments
+     * @return $this
+     */
+    public function __call($name, $arguments)
     {
-        $this->image['alt'] = 'alt="' . $alt . '"';
-    }
-
-    public function height($height)
-    {
-        $this->image['height'] = 'height="' . $height . '"';
-
-        return $this;
-    }
-
-    public function width($width)
-    {
-        $this->image['width'] = 'width="' . $width . '"';
+        $arg = isset($arguments[0]) ? filter_var($arguments[0], FILTER_SANITIZE_SPECIAL_CHARS) : '';
+        $this->attribute[$name] = $name . '="' . $arg . '"';
 
         return $this;
     }
 
     public function __toString()
     {
-        if (!isset($this->image['alt'])) {
-            $this->image['alt'] = 'alt=""';
+        if (!isset($this->attribute['alt'])) {
+            $this->attribute['alt'] = 'alt=""';
         }
 
-        return '<img ' . implode(' ', $this->image) . '/>';
+        return '<img ' . implode(' ', $this->attribute) . '>';
+    }
+
+    public function hidden()
+    {
+        $this->attribute['hidden'] = 'hidden';
+        return $this;
     }
 }
