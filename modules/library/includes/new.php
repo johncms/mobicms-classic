a111d97652e22ed60244059fd1e780c2ef8091a3
Oleg Kasyanov <dev@mobicms.net>
2017-07-30 16:23:04 +0400

    Исправлена ошибка в отображении меню навигации по страницам

--- modules/library/includes/new.php
+++ modules/library/includes/new.php
@@ -30,16 +30,18 @@ echo '<div class="phdr"><strong><a href="?">' . _t('Library') . '</a></strong> |
 $total = $db->query("SELECT COUNT(*) FROM `library_texts` WHERE `time` > '" . (time() - 259200) . "' AND `premod`=1")->fetchColumn();
 
 if ($total) {
+    $page = $page >= ceil($total / $userConfig->kmess) ? ceil($total / $userConfig->kmess) : $page;
+    $start = $page == 1 ? 0 : ($page - 1) * $userConfig->kmess;
 
-$page = $page >= ceil($total / $userConfig->kmess) ? ceil($total / $userConfig->kmess) : $page;
-$start = $page == 1 ? 0 : ($page - 1) * $userConfig->kmess;
-$nav = ($total > $userConfig->kmess) ? '<div class="topmenu">' . $tools->displayPagination('?act=new&amp;', $total) . '</div>' : '';
-echo $nav;
-$sql = $db->prepare("SELECT libtxt.`id`, libtxt.`name`, libtxt.`time`, `uploader`, `uploader_id`, `count_views`, `comments`, `comm_count`, `cat_id`, `announce`, libcat.name as catName
+    if ($total > $userConfig->kmess) {
+        echo '<div class="topmenu">' . $tools->displayPagination('?act=new&amp;', $total) . '</div>';
+    }
+
+    $sql = $db->prepare("SELECT libtxt.`id`, libtxt.`name`, libtxt.`time`, `uploader`, `uploader_id`, `count_views`, `comments`, `comm_count`, `cat_id`, `announce`, libcat.name AS catName
                     FROM `library_texts` libtxt
                     JOIN library_cats libcat ON libtxt.cat_id=libcat.id
                     WHERE libtxt.`time`>? AND `premod`=? ORDER BY libtxt.`time` DESC LIMIT " . $start . "," . $userConfig->kmess);
-$sql->execute([(time() - 259200), 1]);
+    $sql->execute([(time() - 259200), 1]);
     $i = 0;
     while ($row = $sql->fetch()) {
         echo '<div class="list' . (++$i % 2 ? 2 : 1) . '">'
@@ -86,7 +88,14 @@ $sql->execute([(time() - 259200), 1]);
 
         echo '</div>';
     }
+} else {
+    echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
 }
-echo '<div class="phdr">' . _t('Total') . ': ' . intval($total) . '</div>';
-echo $nav;
+
+echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
+
+if ($total > $userConfig->kmess) {
+    echo '<div class="topmenu">' . $tools->displayPagination('?act=new&amp;', $total) . '</div>';
+}
+
 echo '<p><a href="?">' . _t('To Library') . '</a></p>';
