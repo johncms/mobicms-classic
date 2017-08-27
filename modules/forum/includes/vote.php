6c6d3e6151495f38c1d58e4027c451b0c8c01d15
ua0sqq <ua0sqq@rambler.ru>
2017-08-27 21:46:36 +0800

    Forum vote - undefined index vote

--- modules/forum/includes/vote.php
+++ modules/forum/includes/vote.php
@@ -17,20 +17,37 @@ defined('MOBICMS') or die('Error: restricted access');
  * @var Mobicms\Api\UserInterface $systemUser
  */
 
-if ($systemUser->isValid()) {
-    $topic = $db->query("SELECT COUNT(*) FROM `forum` WHERE `type`='t' AND `id` = '$id' AND `edit` != '1'")->fetchColumn();
-    $vote = abs(intval($_POST['vote']));
-    $topic_vote = $db->query("SELECT COUNT(*) FROM `cms_forum_vote` WHERE `type` = '2' AND `id` = '$vote' AND `topic` = '$id'")->fetchColumn();
-    $vote_user = $db->query("SELECT COUNT(*) FROM `cms_forum_vote_users` WHERE `user` = '" . $systemUser->id . "' AND `topic` = '$id'")->fetchColumn();
+$vote = filter_input(INPUT_POST, 'vote', FILTER_VALIDATE_INT);
+
+if ($vote && $id && $systemUser->isValid()) {
+    $smtp = $db->prepare('SELECT * FROM (
+                          SELECT COUNT(*) as `topic` FROM `forum` WHERE `type`=? AND `id`=? AND `edit`<>?) q1, (
+                          SELECT COUNT(*) as `topic_vote` FROM `cms_forum_vote` WHERE `type`=? AND `id`=? AND `topic`=?) q2, (
+                          SELECT COUNT(*) as `vote_user` FROM `cms_forum_vote_users` WHERE `user`=? AND `topic`=?) q3
+                          ');
+    $smtp->execute(['t', $id, 1, 2, $vote, $id, $systemUser->id, $id]);
+    $cnt = $smtp->fetch();
 
     ob_start();
 
-    if ($topic_vote == 0 || $vote_user > 0 || $topic == 0) {
-        exit(_t('Wrong data'));
+    if ($cnt['topic_vote'] == 0 || $cnt['vote_user'] > 0 || $cnt['topic'] == 0) {
+        echo $view->render(
+                           'system::app/legacy', [
+                                                  'title'   => _t('Forum'),
+                                                  'content' => $tools->displayError(_t('Wrong data'), '<a href="' . htmlspecialchars(getenv('HTTP_REFERER')) . '">' . _t('Back') . '</a>'),
+                                                  ]);
+        exit;
     }
 
     $db->exec("INSERT INTO `cms_forum_vote_users` SET `topic` = '$id', `user` = '" . $systemUser->id . "', `vote` = '$vote'");
     $db->exec("UPDATE `cms_forum_vote` SET `count` = count + 1 WHERE id = '$vote'");
     $db->exec("UPDATE `cms_forum_vote` SET `count` = count + 1 WHERE topic = '$id' AND `type` = '1'");
     echo _t('Vote accepted') . '<br /><a href="' . htmlspecialchars(getenv("HTTP_REFERER")) . '">' . _t('Back') . '</a>';
+} else {
+    echo $view->render(
+                       'system::app/legacy', [
+                                              'title'   => _t('Forum'),
+                                              'content' => $tools->displayError(_t('Wrong data'), '<a href="' . htmlspecialchars(getenv('HTTP_REFERER')) . '">' . _t('Back') . '</a>'),
+                                              ]);
+    exit;
 }
